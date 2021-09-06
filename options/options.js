c3a4b1a940af9fd8f82dc0fc52d55a525eed861f
Crowdbotics <team@crowdbotics.com>
2021-09-06 16:32:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "prexplay_30358Identifier",
+  url: "https://prexplay_30358Identifier.botics.co",
+  api: "https://prexplay_30358Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
